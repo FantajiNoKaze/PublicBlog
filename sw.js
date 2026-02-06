dbd2e21ccee3112d5a92e74bdb2e306c71e6719b
FantajiNoKaze <fantasy.for.games@gmail.com>
2026-02-07 01:29:22 +0800

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770398460174";const preCache = ["/PublicBlog/images/taichi.png","/PublicBlog/images/banner.webp","/PublicBlog/css/loader.css","/PublicBlog/css/style.css","/PublicBlog/js/script.js"];const cacheDomain = [
+const VERSION = "1770398897986";const preCache = ["/PublicBlog/images/taichi.png","/PublicBlog/images/banner.webp","/PublicBlog/css/loader.css","/PublicBlog/css/style.css","/PublicBlog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
