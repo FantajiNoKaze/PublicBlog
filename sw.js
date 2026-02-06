c76be3665984f9cdd1448a700516893f0d3e884c
FantajiNoKaze <fantasy.for.games@gmail.com>
2026-02-07 01:21:48 +0800

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770397623581";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770398460174";const preCache = ["/PublicBlog/images/taichi.png","/PublicBlog/images/banner.webp","/PublicBlog/css/loader.css","/PublicBlog/css/style.css","/PublicBlog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
